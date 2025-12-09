e21233d46aa0925d5dfe331a25206bd1c336d4d6
Emmanuel .A <asaoluemma@gmail.com>
2025-12-09 16:48:50 +0100

    mesh-txbuilder

---
+++ mesh-backend-tx/src/lib.ts
@@ -0,0 +1,22 @@
+import { BlockfrostProvider, MeshWallet } from "@meshsdk/core"
+import "dotenv/config"
+
+export const seed_phrase = process.env.SEED_PHRASE?.split(" ")
+if(!seed_phrase){
+    throw new Error("Enter seedphrase in .env file")
+}
+ const provider = process.env.BLOCKFROST_APIKEY;
+if(!provider){
+    throw new Error(" blockchain provider not set")
+}
+export const blockchain_provider = new BlockfrostProvider(provider);
+
+export const wallet = new MeshWallet({
+    networkId: 0,
+    fetcher: blockchain_provider,
+    submitter: blockchain_provider,
+    key: {
+      type: "mnemonic",
+      words: seed_phrase
+    }
+})
