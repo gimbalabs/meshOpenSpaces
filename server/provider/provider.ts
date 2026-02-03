ed8405ec5b94a56453d385f61158f3e80f6118a6
gZero <gullafourteen@gmail.com>
2026-02-03 10:18:21 -0500

    deploy ref script

--- server/provider/provider.ts
+++ server/provider/provider.ts
@@ -1,6 +1,7 @@
 import { BlockfrostProvider } from "@meshsdk/core";
 
-const provider = process.env.BLOCKFROST_APIKEY;
+const provider = process.env.BLOCKFROST;
+console.log("[provider] BLOCKFROST set:", !!provider, "length:", provider?.length ?? 0);
 if(!provider){
     throw new Error(" blockchain provider not set")
 }
